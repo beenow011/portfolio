c947196334ef9915f5076cc89ecd2e5df8ca9c7b
beenow011 <abhinavnb11@gmail.com>
2024-01-13 19:41:58 +0530

    color correction

--- src/components/skills/SkillComponent.jsx
+++ src/components/skills/SkillComponent.jsx
@@ -6,7 +6,7 @@ function SkillComponent({ lang, icon }) {
   const theme = useSelector((state) => state.theme);
   return (
     <motion.div
-      className={`flex md:flex-col justify-center items-center ${
+      className={`flex md:flex-col justify-center items-center w-64 md:w-full rounded-md ${
         theme === "light"
           ? "bg-gradient-to-r from-rose-500 via-red-400 to-red-500"
           : "bg-gradient-to-r from-gray-700 via-gray-900 to-black text-white"
@@ -17,7 +17,13 @@ function SkillComponent({ lang, icon }) {
       <p className=" font-semibold p-3 w-full md:w-[100px] md:h-[100px] text-wrap">
         {lang}
       </p>
-      <img src={icon} alt={lang} width={100} height={100} className="" />
+      <img
+        src={icon}
+        alt={lang}
+        width={100}
+        height={100}
+        className="w-12 md:w-[100px]"
+      />
     </motion.div>
   );
 }
